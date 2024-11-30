46aee83ee5b7b71ab8bc539631721f3b207f17e7
Erik Martin <erikmartinfernandez@gmail.com>
2024-11-30 10:13:44 +0000

    css update

--- src/js/views/contacts.js
+++ src/js/views/contacts.js
@@ -10,28 +10,34 @@ export const Contacts = () => {
     const  { store } = useContext(Context)
     
     return (
-        <div className="text-center mt-5 container">
-            <h1 className="text-light mb-5">Contacts</h1>
-            <ul>
+        <div className=" mt-5 container">
+            <h1 className="text-center text-light mb-5">Contacts</h1>
+    
                 {
 
-                store.contacts.map((item) => (
-                    <li className="text-light mb-4" key={item}>
-                        {item.name}
-                        {item.phone}
-                        {item.email}
-                        {item.address}
+                store.contacts.map((contact) => (
+                    <div className="d-flex justify-content-between contact mx-auto" key={contact}>
+                        <div className="user my-auto"><i className="fa-solid fa-user user1"></i></div>
+                        <div className="d-block">
+                        <div className="mb-2"><h5>{contact.name}</h5></div>
+                        <div className="mb-2"><i class="fa-solid fa-phone"></i> +34 {contact.phone}</div>
+                        <div className="mb-2"><i class="fa-solid fa-envelope"></i> {contact.email}</div>
+                        <div><i className="mb-2 fa-solid fa-location-dot"></i> {contact.address}</div>
+                        </div>
+                        <div>
+                            <button className="btn btn-dark border border-warning m-1" onClick={() => {}}><i class="fa-solid fa-pen"></i></button>
+                            <button className="btn btn-dark border border-danger m-1"onClick={() => {}}><i class="fa-solid fa-x"></i></button>
+                        </div>
 
-
-                    </li>
+                    </div>
                 ))
 
                 }
-            </ul>
-            <Link to="/new-contact" className="btn btn-light">
+            <div className="text-center">
+            <Link to="/new-contact" className="text-center btn btn-light mt-3">
             Add new contact
             </Link>
-           
+            </div>
         </div>
     )
 };
--- src/js/views/newContact.js
+++ src/js/views/newContact.js
@@ -17,9 +17,22 @@ export const NewContact = () => {
 
     <form className="form mx-auto">
       <div className="mb-3">
-        <label for="Name" className="form-label text-light">Name</label>
+        <label for="name" className="form-label text-light">Name</label>
         <input type="text" className="form-control" />
       </div>
+      <div className="mb-3">
+        <label for="phone" className="form-label text-light">Phone</label>
+        <input type="text" className="form-control" />
+      </div>
+      <div className="mb-3">
+        <label for="email" className="form-label text-light">Email</label>
+        <input type="text" className="form-control" />
+      </div>
+      <div className="mb-3">
+        <label for="adress" className="form-label text-light">adress</label>
+        <input type="text" className="form-control" />
+      </div>
+
       
       <div className="d-flex justify-content-between">
         <Link to="/" className="btn btn-success button2">Go back home</Link>
